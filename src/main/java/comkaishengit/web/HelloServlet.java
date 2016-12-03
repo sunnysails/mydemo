43e73159af02ff2cd405c259d9d89c180e9936e4
sunnysails <sunnysails@qq.com>
2016-12-03 14:44:23 +0800

    新建

---
+++ src/main/java/comkaishengit/web/HelloServlet.java
@@ -0,0 +1,20 @@
+package comkaishengit.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by sunny on 2016/12/3.
+ */
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("hello，maven");
+        req.getRequestDispatcher("/WEB-INF/views/hello.jsp").forward(req,resp);
+    }
+}
